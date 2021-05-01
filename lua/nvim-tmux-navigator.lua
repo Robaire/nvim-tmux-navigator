ea07c9e5dbaeed5fd6609e091ac5f2ecf2449e12
Robaire Galliath <galliath.robaire@gmail.com>
2021-05-01 17:14:25 -0400

    Implement window switching

--- lua/nvim-tmux-navigator.lua
+++ lua/nvim-tmux-navigator.lua
@@ -18,25 +18,35 @@ end
 -- Must be global so it is seen by the remap
 function tmuxAwareNavigate(direction)
 
-    -- Check if we are moving out of the last pane
-    -- Save the current window number
-    local winNumber = vim.fn.winnr()
+    -- Check the direction to move
+    if direction == 'p' then
+        tmuxNavigate('p')
+    else
+        -- Check if we are moving out of the last pane
+        -- Save the current window number
+        local winNumber = vim.fn.winnr()
 
-    -- Attempt to move to a new window
-    vim.cmd('wincmd ' .. direction)
+        -- Attempt to move to a new window
+        vim.cmd('wincmd ' .. direction)
 
-    -- Check if we actually moved
-    if winNumber == vim.fn.winnr() then
-
-        -- Get the TMUX socket
-        local sock = vim.fn.split(vim.fn.getenv('TMUX'), ',')[1]
-        local pane = vim.fn.getenv('TMUX_PANE')
-
-        -- Forward the move command to TMUX
-        local cmd = 'tmux -S ' .. sock .. ' select-pane -t ' .. pane .. ' -' .. vim.fn.tr(direction, 'phjkl', 'lLDUR')
-        local result = vim.fn.system(cmd)
+        -- Check if we actually moved
+        if winNumber == vim.fn.winnr() then
+            tmuxNavigate(direction)
+        end
     end
 end
+
+local function tmuxNavigate(direction)
+
+    -- Get the TMUX socket
+    local sock = vim.fn.split(vim.fn.getenv('TMUX'), ',')[1]
+    local pane = vim.fn.getenv('TMUX_PANE')
+    
+    -- Forward the move command to TMUX
+    local cmd = 'tmux -S ' .. sock .. ' select-pane -t ' .. pane .. ' -' .. vim.fn.tr(direction, 'phjkl', 'lLDUR')
+    local result = vim.fn.system(cmd)
+end
+
 
 local function setup(config)
 
@@ -49,15 +59,20 @@ local function setup(config)
 
     -- Check if NVIM is running in a TMUX environment
     if vim.fn.getenv('TMUX') == vim.NIL then
+
+        -- Set the regular key bindings
         vim.api.nvim_set_keymap('n', bindings.left, ':wincmd h<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.down, ':wincmd j<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.up, ':wincmd k<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.right, ':wincmd l<CR>', {noremap = true, silent = true})
     else
+
+        -- Set the TMUX aware key bindings
         vim.api.nvim_set_keymap('n', bindings.left, ':lua tmuxAwareNavigate(\'h\')<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.down, ':lua tmuxAwareNavigate(\'j\')<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.up, ':lua tmuxAwareNavigate(\'k\')<CR>', {noremap = true, silent = true})
         vim.api.nvim_set_keymap('n', bindings.right, ':lua tmuxAwareNavigate(\'l\')<CR>', {noremap = true, silent = true})
+        vim.api.nvim_set_keymap('n', bindings.previous, ':lua tmuxAwareNavigate(\'p\')<CR>', {noremap = true, silent = true})
     end
 end
 
